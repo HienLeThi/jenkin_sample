5e9fe8a4c95d7856d421a6f33d4faae11b6c12da
Le Thi Hien <le.thi.hien@framgia.com>
2015-03-19 13:57:04 +0700

    initial commit

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.<span id="IL_AD9" class="IL_AD">annotations</span>.Test;
+
+/**
+ * @author Gaurang_Shah
+ */
+public class GoogleTest {
+ private WebDriver driver;
+
+// @Test
+ public void verifySearch() {
+  driver = new FirefoxDriver();
+  driver.get("http://www.google.com/");
+ // driver.quit();
+ }
+}
